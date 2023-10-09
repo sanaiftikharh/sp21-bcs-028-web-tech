54b9046867449ef46561c4caff332348469c9501
sanaiftikharh <sanaiftikharh@gmail.com>
2023-10-09 16:26:52 +0500

    express-ejs-layout

--- Practice/Express-app/server.js
+++ Practice/Express-app/server.js
@@ -1,13 +1,33 @@
 const express = require("express");
 const app = express();
+var expressLayouts = require("express-ejs-layouts");
 //const port = process.env.PORT || 3000;
 
 // Middleware to serve static files from the "public" directory
 app.use(express.static("public"));
-
+app.set("view engine", "ejs");
+app.use(expressLayouts);
 // Set up routes (You can create separate route files in the "routes" folder)
-app.get("/", (req, res) => {
-  res.send("Hello, Express!");
+app.get("/", function (req, res) {
+  res.render("landing-page");
+});
+app.get("/stitched", function (req, res) {
+  res.render("products/stitched");
+});
+app.get("/unstitched", function (req, res) {
+  res.render("products/unstitched");
+});
+app.get("/all", function (req, res) {
+  res.render("products/all");
+});
+app.get("/new-arrival", function (req, res) {
+  res.render("products/new-arrival");
+});
+app.get("/cart", function (req, res) {
+  res.render("cart");
+});
+app.get("/contact-us", function (req, res) {
+  res.render("contact-us");
 });
 
 // Start the server
